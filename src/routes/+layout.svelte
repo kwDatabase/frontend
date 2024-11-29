f4286439d4e6018181d778f0fdd71f903edfde10
LeeHoYun <68680118+HaTToek@users.noreply.github.com>
2024-11-29 20:45:59 +0900

    flowbite-svelte 적용 및 navigationbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/admin">Admin</a>
-  </nav>
-  
-  <slot></slot>
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
+  import { SearchOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Navbar>
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Market</span>
+  </NavBrand>
+  <div class="flex md:order-2">
+    <Button color="none" data-collapse-toggle="mobile-menu-2" aria-controls="mobile-menu-2" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+      <SearchOutline class="w-5 h-5" />
+    </Button>
+    <div class="hidden relative md:block">
+      <div class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none">
+        <SearchOutline class="w-4 h-4" />
+      </div>
+      <Input id="search-navbar" class="ps-10" placeholder="Search..." />
+    </div>
+    <NavHamburger />
+  </div>
+  <NavUl>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/admin">Admin</NavLi>
+  </NavUl>
+</Navbar>
+<slot></slot>
