e5a8a3e4a4713cfa7712729e87c9558bbed50b57
LeeHoYun <68680118+HaTToek@users.noreply.github.com>
2024-12-04 21:53:53 +0900

    사이드 바 메뉴 acitve 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,14 @@
 </script>
 
 <div class="h-screen">
-  <Navbar class="border-b border-gray-700">
+  <Navbar class="border-b border-gray-300 dark:border-gray-700">
     <NavBrand href="/">
       <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Market</span>
     </NavBrand>
     <div class="flex md:order-2">
-      <Button color="none" data-collapse-toggle="mobile-menu-2" aria-controls="mobile-menu-2" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+      <Button color="none" data-collapse-toggle="mobile-menu-2" aria-controls="mobile-menu-2" 
+              aria-expanded="false" 
+              class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
         <SearchOutline class="w-5 h-5" />
       </Button>
       <div class="hidden relative md:block">
--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,13 +1,18 @@
 <script>
   import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import { page } from '$app/stores';
   
   const routes = [
-    { label: '사용자', href: '/admin/user' },
+    { label: '사용자 관리', href: '/admin/user' },
     { label: '카테고리 관리', href: '/admin/categori' },
     { label: '권한 관리', href: '/admin/auth' },
     { label: '상품 관리', href: '/admin/product' },
     { label: '대시보드', href: '/admin/dashboard' },
   ];
+	$: activeUrl = $page.url.pathname;
+
+	let spanClass = 'pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white';
+  var activeClass = "flex items-center p-2 text-base font-normal text-gray-900 bg-primary-200 dark:bg-primary-700 rounded-lg dark:text-white hover:bg-primary-100 dark:hover:bg-primary-700";
 </script>
 
 <style>
@@ -16,7 +21,7 @@
     height: 100%;
   }
   .sidebar {
-    min-width: 150px;
+    min-width: 100px;
     height: 100%;
     display: flex;
     flex-direction: column;
@@ -28,13 +33,14 @@
   }
 </style>
 
-<main class="layout-container">
+<main class="layout-container" >
   <div class="sidebar">
-    <Sidebar class="h-full bg-gray-50 dark:bg-gray-800 dark:text-white border-r dark:border-gray-700">
-      <SidebarWrapper>
-        <SidebarGroup>
+    <Sidebar class="h-full bg-white dark:bg-gray-900 dark:text-white border-r dark:border-gray-700"
+             {activeUrl}>
+      <SidebarWrapper class="h-full dark:bg-gray-900 dark:text-white">
+        <SidebarGroup class="side-menu-item">
           {#each routes as { label, href }}
-            <SidebarItem {label} {href} />
+            <SidebarItem {label} {href} {spanClass}/>
           {/each}
         </SidebarGroup>
       </SidebarWrapper>
