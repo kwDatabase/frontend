c2a7eb2ada5b16b5d788232280d33c61b38fb399
LeeHoYun <68680118+HaTToek@users.noreply.github.com>
2024-12-06 01:31:04 +0900

    revert

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -7,28 +7,17 @@ export const nodes = [
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
-	() => import('./nodes/6'),
-	() => import('./nodes/7'),
-	() => import('./nodes/8'),
-	() => import('./nodes/9'),
-	() => import('./nodes/10'),
-	() => import('./nodes/11'),
-	() => import('./nodes/12')
+	() => import('./nodes/6')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [3],
-		"/admin": [~4,[2]],
-		"/admin/auth": [5,[2]],
-		"/admin/categori": [6,[2]],
-		"/admin/dashboard": [7,[2]],
-		"/admin/product": [8,[2]],
-		"/admin/user": [9,[2]],
-		"/join": [10],
-		"/login": [11],
-		"/mypage": [12]
+		"/": [2],
+		"/admin": [3],
+		"/join": [4],
+		"/login": [5],
+		"/mypage": [6]
 	};
 
 export const hooks = {
